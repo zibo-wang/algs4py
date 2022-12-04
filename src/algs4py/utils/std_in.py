2319a7b5989ca197a55e5969797632146ae0cc2d
Zibo Wang <zibo.w@outlook.com>
2022-12-05 00:13:41 +1100

    refactor stdin to use a queue

--- src/algs4py/utils/std_in.py
+++ src/algs4py/utils/std_in.py
@@ -1,9 +1,15 @@
-"""Overview
+"""StdIn
+
+StdIn is a utility class that provides methods for reading from standard input.
+We simulate the Java like StdIn using a queue.
 
 """
 
 import sys
-from typing import TextIO
+from queue import Queue
+from typing import Any, Iterator, List, Optional, TextIO
+
+import numpy as np
 
 from algs4py.utils.std_out import StdOut
 
@@ -18,71 +24,68 @@ class StdIn:
         # Assume language = English, country = AU for consistency with System.out.
         self.LOCALE: str = "en_AU"
 
-        self.input: TextIO = sys.stdin
+        self._input: TextIO = sys.stdin
 
-        self.cursor: int = 0
+        self.tokens: Queue[str] = Queue()
+
+        self.__stdin2queue()
+
+    def __stdin2queue(self) -> None:
+        """Read from stdin and put into queue."""
+        for line in self._input:
+            for token in line:
+                self.tokens.put(token)
 
     def is_empty(self) -> bool:
-        """Check if the std in is empty.
+        """Is the queue empty?
+
+            Use this method to check if next to read_string(), read_double() etc.
+            will succeed.
 
         Returns:
-            bool: True if the standard input is empty, False otherwise.
+            bool: True if the queue is empty, False otherwise.
         """
-        return not sys.stdin.readable()
+        return self.tokens.empty()
 
     def has_next_line(self) -> bool:
-        """Check if the std in has next line.
+        """Does the input have more lines?
+
+            Use this method to check if read_line() will succeed.
 
         Returns:
-            bool: True if the standard input has next line, False otherwise.
+            bool: True if the input has more lines, False otherwise.
         """
-
-        try:
-            self.input.seek(self.cursor)
-            self.input.readline()
-            self.cursor = self.input.tell()
-            return True
-        except:
-            return False
-
+        return not self.is_empty()
     def has_next_char(self) -> bool:
-        """Check if the std in has next char.
+        """Does the input have more characters?
+
+            Use this method to check if read_char() will succeed.
 
         Returns:
-            bool: True if the standard input has next char, False otherwise.
+            bool: True if the input has more characters, False otherwise.
         """
-        try:
-            self.input.seek(self.cursor)
-            self.input.read(1)
-            self.cursor = self.input.tell()
-            return True
-        except:
-            return False
+        return not self.is_empty()
 
-    def read_line(self) -> str:
-        """Read a line from the std in.
+    def read_line(self) -> Optional[str]:
+        """Reads the next line of text from standard input and returns it as a string.
 
         Returns:
-            str: A line from the standard input.
+            Optional[str]: The next line of text from standard input as a string.
         """
-        return self.input.readline()
+        if self.tokens.empty():
+            return None
+        else:
+            return self.tokens.get()
 
-    def read_char(self) -> str:
-        """Read a char from the std in.
-
-        Returns:
-            str: A char from the standard input.
-        """
-        return self.input.read(1)
-
-    def read_string(self) -> str:
-        """Read a string from the std in.
-
-        Returns:
-            str: A string from the standard input.
-        """
-        return str(self.input.read())
+    def print_queue(self) -> None:
+        """Print the queue."""
+        l: List[str] = []
+        while not self.tokens.empty():
+            l.append(self.tokens.get())
+        print(l)
+        self.__stdin2queue()
 
 
 if __name__ == "__main__":
-    StdOut.print("Type")
+    StdOut().print("Type a string: ")
+    StdIn().print_queue()
